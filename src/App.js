6ee63bdaba563cc34eec0ab52d84c39bba11285e
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-09-10 22:26:26 +0200

    +

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import Register from "./components/Register"
+import "./style.scss"
+
+function App() {
+  return (
+    <Register/>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+// https://www.youtube.com/watch?v=k4mjF4sPITE 14:11
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
